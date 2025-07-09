4779b160cbf382fe5bff4ea1a10af5fdb1b6f799
clementbiteau <clement.biteau.mars@gmail.com>
2025-07-09 17:55:19 +0200

    improving list comprehension on ex06 + filter output + inscope function

--- Python0/ex06/filterstring.py
+++ Python0/ex06/filterstring.py
@@ -1,28 +1,35 @@
-from ft_filter import ft_filter
 import sys
+import string
+from ft_filter import ft_filter
 
 def main():
     """main() defined to take a string and an integer.
     We first split the string and appropriately use the lambda anonymous function that will
     check if the len of a word is greater than N"""
-    if len(sys.argv) == 3:
+    def check_input(text: str)->bool:
+        """checking the input if there are any non accepted format chars like punctuation as per constraints"""
+        for w in text:
+            if w in string.punctuation:
+                return False
+        return True
+    if len(sys.argv) != 3:
+        print("AssertionError: the arguments are bad")
+        return
+    try:
+        text = sys.argv[1]
         try:
-            S = str(sys.argv[1])
-            N = int(sys.argv[2])
+            min_len = int(sys.argv[2])
         except ValueError:
-            raise AssertionError("could not evaluate input")
-        
-        # Here we use list comprehension
-        # we store in result_l_c a list that takes a word (split from string S) if the len of the word is greater than N.
-        result_list_comprehension = [word for word in S.split() if (lambda x: len(x) > N)(word)]
-        print(result_list_comprehension)
+            raise AssertionError("the arguments are bad")
+        if not check_input(text) or min_len < 0:
+            raise AssertionError("the arguments are bad")
 
-        # Here we use ft_filter(), our own function to perform the same operation
-        # We store in result_list a created list using the ft_filter()
-        result_list = list(ft_filter(lambda word: len(word) > N, S.split()))
-        print(result_list)
-    else:
-        raise AssertionError("the arguments are bad")
+        ft = ft_filter(lambda w: len(w) > min_len, text.split())
+        print(ft)
+        
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
 
 if __name__ == "__main__":
     main()
--- Python0/ex06/ft_filter.py
+++ Python0/ex06/ft_filter.py
@@ -1,5 +1,7 @@
 def ft_filter(function, params):
-    """filter() function re coded"""
+    """filter() function re coded
+    We filter for function() by the given params.
+    If the function is none it returns truthy item"""
     if function is None:
-        return (item for item in params if item)
-    return (item for item in params if function(item))
+        return [item for item in params if item]
+    return [item for item in params if function(item)]
